8e54ae394a39bd5a0286ead8c93f60ed7f0b040e
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-11 16:17:48 -0500

    taking in hser input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// hello world
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+//taking in user data
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Enter you're guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You're guess: {guess}");
 }
