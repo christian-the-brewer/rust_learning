14fdbf9463a8c8f4d11e80674389269300471163
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-15 19:57:03 -0500

    section on if statements

--- src/main.rs
+++ src/main.rs
@@ -40,30 +40,51 @@
 //     }
 // }
 
-// shadowing and tuples and arrays
+// // shadowing and tuples and arrays
+// fn main() {
+//     let x = 5;
+//     let x = x + 1;
+//     {
+//         let x = x * 2;
+//         println!("the value of x in the inner scope is {x}");
+//     }
+
+//     println!("the value of x is {x}");
+
+//     let tup = (500, 6.4, 1);
+
+//     let (x, y, z) = tup;
+
+//     println!("the value of y is {y}");
+
+//     let x: (i32, f64, u8) = (500, 6.4, 1);
+
+//     let five_hundred = x.0;
+
+//     println!("five hundred = {five_hundred}");
+
+//     let a = [1, 2, 3, 4, 5];
+
+//     println!("{} {}", a[0], a[1]);
+// }
+
+// if expressions
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    {
-        let x = x * 2;
-        println!("the value of x in the inner scope is {x}");
+    // let number = 3;
+
+    // if number < 5 {
+    //     println!("condition was true");
+    // } else {
+    //     println!("condition was false");
+    // }
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
     }
-
-    println!("the value of x is {x}");
-
-    let tup = (500, 6.4, 1);
-
-    let (x, y, z) = tup;
-
-    println!("the value of y is {y}");
-
-    let x: (i32, f64, u8) = (500, 6.4, 1);
-
-    let five_hundred = x.0;
-
-    println!("five hundred = {five_hundred}");
-
-    let a = [1, 2, 3, 4, 5];
-
-    println!("{} {}", a[0], a[1]);
 }
